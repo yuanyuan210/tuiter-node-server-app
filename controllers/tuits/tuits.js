bff0ed371025b63a261f42fd15d7c6a8f8a83c05
yuanyuan <wu.yuanyu@northeastern.edu>
2022-12-01 19:45:49 -0800

    a9 fix bug

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -15,4 +15,17 @@ export default [
         "liked": true, "likes": 2345, "dislikes": 0,
         "replies": 123, "retuits": 432
     },
+    {
+        "_id": "456", "topic": "SpaceX",
+        "userName": "SpaceX",
+        "verified": false,
+        "time": "2h",
+        "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
+        "logo": "../../../images/spacex.jpg",
+        "handle": "spacex",
+        "tuit": "Tesla Cybertruck lands on Mars and picks up the Curiosity rover on its 6' bed. Cybertruck is built with an extenior shell made for ultimate durability and passenger protection. Starting with a nearly impenetrable exoskeleton, every component is designed for superior strength and endurance, from Ultra-hard 30X Cold-Rolled stainless-steel structural skin to Tesla armor glass. Cybertruck is built with an exterior shell made for ultimate durability and passenger protection. Staring with a nearly impenetrable exoskeleton, every component is designed for superior strength and endurance, from Ultra-hard 30X Clod-Rolled stainless-steel structural skin to Tesla armor glass.",
+        "liked": true,
+        "likes": 2345, "dislikes": 2,
+        "replies": 123, "retuits": 432
+    }
 ];
